70b051450f9d27baf493c035c0b2c648e673014d
Sebastian Hancock <sebastian.hancock@tum.de>
2020-12-28 10:20:35 -0800

    stopped it

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 var url = 'http://www.followmee.com/api/tracks.aspx?key=6ff60367d512c2e80cffb15a08c997a9&username=sebastianch&output=json&function=currentforalldevices';
 
 
-getMyData();
-setInterval(getMyData, 300000)
+//getMyData();
+//setInterval(getMyData, 300000)
 
 function getMyData() {
 
